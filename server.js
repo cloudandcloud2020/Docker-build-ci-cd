1171eb8987db005b04429c3314176dc75cfb7a99
root <root@ip-172-31-89-30.ec2.internal>
2020-11-22 06:32:00 +0000

    added js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello This Project Deployed Via Jenkins By Sankara \n');
+  res.send('Hello This Project Deployed Via Jenkins By Sankara! \n');
 });
 
 app.listen(PORT, HOST);
